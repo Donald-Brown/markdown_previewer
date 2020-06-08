198fb840eac88c32a4f969a81b9448bad421bdf3
Donald Brown <donbrown4652@gmail.com>
2020-06-08 15:16:50 -0500

    completes project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import marked from "marked";
+import initMarkUp from './components/initMarkUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    text: initMarkUp,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  render() {
+    const { text } = this.state;
+
+    const markdown = marked(text, {breaks: true});
+
+    return (
+      <div>
+        <h2 className="text-center m-4">Markdown Preview</h2>
+        <div className="row">
+          <div className="col-md-6">
+            <h5 className="text-center">Enter Markdown Text Here:</h5>
+            <textarea
+              className="editor form-control"
+              id="editor"
+              value={text}
+              onChange={this.handleChange}
+            ></textarea>
+          </div>
+          <div className="col-md-6">
+            <h5 className="text-center">Preview Markdown Here:</h5>
+            <div className="preview p-2 rounded" id="preview" dangerouslySetInnerHTML={{__html: markdown}}>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/initMarkUp.js
@@ -0,0 +1,39 @@
+const initMarkUp = `
+  This is a paragragh
+
+  **This is bold text**
+
+  > Block Quotes!
+
+  # Heading
+  ## Heading 2
+
+  - list item 1
+  - list item 2
+  - list item 3
+
+  [This is a link to Google](https://google.com)
+
+  This is an inline \`<div></div>\`
+
+  \`\`\`
+  /// this is inline code
+  let a = 2;
+  let b = 3;
+  function add(a,b){
+    return a + b;
+  }
+  \`\`\`
+
+  ![Cat Photo](https://images.unsplash.com/photo-1517172527855-d7a4feea491b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60)
+`;
+
+
+export default initMarkUp;
+
+
+
+
+
+
+
